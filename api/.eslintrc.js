2bea22eedffe6169e6c123e6f8a6ae95e4e07d85
Ferran <ferranhendriks@hotmail.com>
2021-03-28 20:20:34 +0200

    Added eslint, WIP eslint errors fixen

---
+++ api/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {
+        /*eslint linebreak-style: ["error", "unix"]*/
+        /*eslint linebreak-style: ["error", "windows"]*/
+    },
+};
