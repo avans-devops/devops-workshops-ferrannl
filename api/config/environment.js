93bfc95e25f56a6b18066732b93ee480af13176c
Ferran <ferranhendriks@hotmail.com>
2021-03-31 17:02:59 +0200

    env variable gemaakt, netwerk gemaakt, data folder gemapped

--- api/config/environment.js
+++ api/config/environment.js
@@ -1,10 +1,10 @@
 const username = process.env.db_username || '';
 const password = process.env.db_password || '';
-const host = process.env.host || 'localhost';
+const host = process.env.DB || 'localhost';
 const port = process.env.port || '27017';
 const database = process.env.database || 'Connections';
 const params = process.env.params || '';
-const secret = process.env.secret || 'secret';
+const secret = process.env.secret || 'MyLittleSecret';
 
 let uri = 'mongodb://';
 if (username && password) {
